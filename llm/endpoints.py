2f56e6f6dbc17b99cb4738e3c02c4e0169d06770
Rene Ravenel <rene@Renes-MacBook-Pro.local>
2024-08-29 15:21:10 -0700

    cleanup

--- llm/endpoints.py
+++ llm/endpoints.py
@@ -8,13 +8,9 @@ DEFAULT_SYSTEM_PROMPT = "The following is a conversation with an AI assistant. T
 
 GPT_4o_MINI = 'gpt-4o-mini'
 GPT_4o_AUG = 'gpt-4o-2024-08-06'
-TEXT_EMBEDDING_3_SMALL = 'text-embedding-3-small' # vector size 1536
-TEXT_EMBEDDING_3_LARGE = 'text-embedding-3-large' # vector size 3072
-TEXT_EMBEDDING_DIMENSION = 1024
 
 TEMPERATURE = 0.0
 DEFAULT_COMPLETE = GPT_4o_MINI
-DEFAULT_EMBED = TEXT_EMBEDDING_3_LARGE
 DEFAULT_RESPONSE_FORMAT = { "type": "json_object" }
 
 def completion(system_prompt: str, user_prompt: str, model: str = DEFAULT_COMPLETE, response_format: dict = DEFAULT_RESPONSE_FORMAT) -> Tuple[str, Any]:
@@ -38,18 +34,10 @@ def completion(system_prompt: str, user_prompt: str, model: str = DEFAULT_COMPLE
     else:
         return None, None # type: ignore
     
-def embed(text: List[str], dimension: int = TEXT_EMBEDDING_DIMENSION, model: str = DEFAULT_EMBED) -> List[str]:
-    response =  client.embeddings.create(
-        model=model,
-        dimension = dimension,
-        input = [text])
-
-    return response.data[0].embedding
-
 def get_cost(usage: dict) -> str:
     if DEFAULT_COMPLETE == GPT_4o_MINI:
-        token_input_cost = .15 / 1000000
-        token_output_cost = .6 / 1000000
+        token_input_cost = .15 / 1000000 # TODO: legacy; update to Usage
+        token_output_cost = .6 / 1000000 # TODO: legacy; update to Usage
     else:
         return '-1.0'
     
